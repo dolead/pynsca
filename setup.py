d0b9439cd58fc4be9849e5527cb3ab4249caa84f
Benoît Latinier <benoit@latinier.fr>
2016-08-12 16:18:14 +0200

    bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ descr = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='pynsca',
-    version='1.6',
+    version='1.6.1',
     description='Simple Python interface to Nagios Service Check Architecture',
     long_description=descr,
     author='Dustin J. Mitchell',
